d64c262dc941483adc9c82403f6a842b72b37e2e
francescopassante <francescopassante@icloud.com>
2026-05-18 17:48:57 +0200

    Built get_task_dict() to get the dictionary that associates a patient to a time series, specified by the task

--- preprocessing.py
+++ preprocessing.py
@@ -36,16 +36,19 @@ def load_data(data_paths, shapes):
     # Count the number of time series in the raw dataset
     initial_runs = 0
     for task, data_path in data_paths.items():
+        # Loop over files in the dataset directory
         for filename in os.listdir(data_path):
             initial_runs += 1
             path = Path(data_path) / filename
+            # Load only the first 6 columns
             data = np.loadtxt(path, usecols=range(6))
-            # Aggiunge i dati al dizionario solo se hanno la shape corretta, altrimenti li scarta
+            # Removes time series with shapes different from the ones specified in <shapes>
             if data.shape == shapes[task]:
                 # Transform degrees to radians for the last 3 columns
                 data[:, 3:] = np.deg2rad(data[:, 3:])
                 # removes ".txt"
                 patient_id = filename[:-4]
+                # builds dictionary entry or updates it if already created
                 patient_dict[patient_id] = patient_dict.get(patient_id, dict())
                 patient_dict[patient_id][task] = data
 
@@ -64,6 +67,18 @@ def load_data(data_paths, shapes):
     return patient_dict
 
 
+def get_task_dict(patient_dict, task):
+    """Returns a dictionary with {patient_id: data} where data is the time series of the specified task"""
+    assert task in ["Resting", "Memory", "Language"], (
+        f"Valid tasks: Resting, Memory or Language, got {task}"
+    )
+    task_dict = {}
+    for id, task in patient_dict.items():
+        task_dict[id] = patient_dict[id][task]
+
+    return task_dict
+
+
 # dictionary with the paths to the three datasets
 data_paths = {
     "Resting": "../RECENTRE-main/HCP/RestingStateLR_dataset",
@@ -78,5 +93,8 @@ shapes = {
     "Language": (316, 6),
 }
 
-# data holds 1027 complete patients (patients that have all the three tasks recorded)
-data = load_data(data_paths, shapes)
+if __name__ == "__main__":
+    # patient_dict holds complete patients (patients that have all the three tasks recorded)
+    patient_dict = load_data(data_paths, shapes)
+    # task_dicts one dictionary for each task, with {patient_id: data}
+    task_dicts = {task: get_task_dict(patient_dict, task) for task in data_paths.keys()}
